d5fdc8b9338440f4f4bdd8362a3254c2ad25481d
Roneison Machado <roneison.machado@gmail.com>
2019-05-09 17:48:20 -0300

    Aula de data-binding

---
+++ src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+  url: string = 'http://machadodefreitas.com';
+  urlImagem: string = 'http://lorempixel.com/400/200/nature/';
+
+  getValor(){
+    return 1;
+  }
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
